1d0e0e64bb27a7d5a2ee2cbe8400eeacb762d568
Sean Mahady <>
2021-05-29 02:06:02 +0000

    no more yeet

--- SimpleGame/Sprite.py
+++ SimpleGame/Sprite.py
@@ -1,1 +1,24 @@
-yeet
+import sys
+from PyQt5.QtGui import QImage, QPainter
+
+
+class Sprite():
+	def __init__(self, thisScene, imageFile, xSize, ySize):
+		self.width=xSize
+		self.height=ySize
+		self.animation = False
+		self.scene = thisScene
+		self.x = 0
+		self.y = 0		
+		
+		# Load our file
+		self.file = QImage(imageFile)
+	
+	def update(self, offX = 0, offY = 0):
+		self.draw(offX, offY)
+		
+	def draw(self, offX, offY): 
+		drawX	= self.x - int(self.width/2) - offX
+		drawY = self.y - int(self.height/2) - offY		
+		qp = QPainter(self.scene)
+		qp.drawImage(drawX, drawY, self.file, )
--- main.py
+++ main.py
@@ -5,7 +5,40 @@ from PyQt5.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
 
-class Game(Scene):
-	pass
 
-print("Test")
+
+class Game(Scene):
+	def __init__(self):
+		super().__init__(600,600)
+		self.spaceship = Sprite(self, "spaceship100.png", 100, 93)
+		
+	def updateGame(self):
+		print("My Update")
+		self.spaceship.x +=1
+		self.spaceship.update()
+
+myGame = Game()
+myGame.start()
+myGame.show()
+sys.exit(app.exec_())
+
+
+# Sean's Sprite Sheet
+# https://opengameart.org/content/cat-fighter-sprite-sheet
+# Cat by DogChicken @ OpenGameArt.org
+
+#Lucas' sprite sheet
+#https://opengameart.org/content/esquire-animated-classic-hero-edit
+#Knight by Umz @http://umzgames.com/
+
+
+#Siqi's sprite sheet
+#
+
+
+#Qingyun's sprite sheet
+#https://opengameart.org/content/cute-monster-sprite-sheet
+
+## Alex's sprite sheet
+#cartoon-rogue by 
+#
